7f102a42d687b8718df3478073fb0aa9a6825017
vleadadmin <admin@vlabs.ac.in>
2024-10-03 06:53:10 +0000

    https://virtual-labs.github.io/virtual-labs/exp-familiarisation-resistor-iitkgp click on the link to test your code.

---
+++ simulation/js/resis.js
@@ -0,0 +1,440 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//------------------------------------------------- Resistor 1  ---------------------------------------//
+//window.onload = function () {
+
+
+function resfn(){
+    var x=document.getElementById("r1").value;
+    var y=document.getElementById("res-select");
+  //  document.getElementById("res-msg").innerHTML=y;
+    var w=y.options[y.selectedIndex].value;
+    var z=document.getElementById("res-tol").value;
+ //   document.getElementById("res-msg").innerHTML=y;
+    
+    if(x==""){
+       document.getElementById("res-msg").innerHTML = "Enter The Resistance "; 
+       document.getElementById("res-msg").style.color="blue";
+     //  document.writeln("<p>Enter The Resistance :</p>");
+    }else if(x=="47"){
+       document.getElementById("res-msg").innerHTML = "Correct Resistance  Value";
+       document.getElementById("res-msg").style.color="green"; 
+      //document.writeln("<p>Correct Resistance  Value</p>");
+    }
+    else if (x!= "47") {
+       document.getElementById("res-msg").innerHTML ="Incorrect resistance value";
+       document.getElementById("res-msg").style.color="red";
+       document.getElementById("res-sbtn").disabled=false;
+        //document.writeln("<p>Incorrect resistance value</p>");
+    }
+     if (w == 0)
+    {
+        document.getElementById("res-msgg").innerHTML ="Please select a unit ";
+       document.getElementById("res-msgg").style.color="blue";
+    }
+    else if (w == "2" ) {
+        document.getElementById("res-msgg").innerHTML ="Correct  Unit Value";
+       
+        document.getElementById("res-msgg").style.color="green";
+    }
+    else if (w != "2") {
+         document.getElementById("res-msgg").innerHTML ="Incorrect Unit";
+         document.getElementById("res-msgg").style.color="red";
+         document.getElementById("res-sbtn").disabled=false;
+          
+    }
+
+   if (z == "") {
+
+        document.getElementById("res-msggg").innerHTML = "Enter the tolerance "; 
+        document.getElementById("res-msggg").style.color="blue";
+        //document.writeln("<p>Enter the tolerance :</p>");
+    }
+    else if (z == "5") {
+        document.getElementById("res-msggg").innerHTML = "Correct Tolerance Value";
+        document.getElementById("res-msggg").style.color="green";
+       // document.writeln("<p>Correct Tolerance Value</p>");
+    }
+    else if (z != "5") {
+        document.getElementById("res-msggg").innerHTML = "Incorrect Tolerance value";
+        document.getElementById("res-msggg").style.color="red";
+        document.getElementById("res-sbtn").disabled=false;
+        //document.writeln("<p>Incorrect Tolerance value</p>");
+    }
+}
+
+//------------------------------------------------- Resistor 2  ---------------------------------------//
+function resfnn(){
+    var x=document.getElementById("r2").value;
+    var y=document.getElementById("res-select2");
+  //  document.getElementById("res-msg2").innerHTML=y;
+    var w=y.options[y.selectedIndex].value;
+    var z=document.getElementById("res-tol2").value;
+ //   document.getElementById("res-msg2").innerHTML=y;
+    
+    if(x==""){
+      document.getElementById("res-msg2").innerHTML = "Enter The Resistance "; 
+      document.getElementById("res-msg2").style.color="blue";
+       // document.writeln("Enter The Resistance :");
+    }else if(x=="5.2"){
+      document.getElementById("res-msg2").innerHTML = "Correct Resistance Value"; 
+      document.getElementById("res-msg2").style.color="green";
+      // document.writeln("Correct Resistance Value");
+    }
+    else if (x!= "5.2") {
+        document.getElementById("res-msg2").innerHTML ="Incorrect resistance value";
+        document.getElementById("res-msg2").style.color="red";
+        document.getElementById("res-sbtn2").disabled=false;
+       // document.writeln("Incorrect resistance value");
+    }
+     if (w == 0)
+    {
+        document.getElementById("res-msgg2").innerHTML ="Please select a unit ";
+        document.getElementById("res-msgg2").style.color="blue";
+        //document.writeln("Please select a unit:");
+    }
+    else if (w == "1") {
+       document.getElementById("res-msgg2").innerHTML ="Correct Unit";
+       document.getElementById("res-msgg2").style.color="green";
+        // document.writeln("Correct Unit Value");
+    }
+    else if (w != "1") {
+         document.getElementById("res-msgg2").innerHTML ="Incorrect Unit";
+         document.getElementById("res-msgg2").style.color="red";
+         document.getElementById("res-sbtn2").disabled=false;
+         // document.writeln("Incorrect Unit");
+    }
+
+   if (z == "") {
+
+        document.getElementById("res-msggg2").innerHTML = "Enter the tolerance "; 
+        document.getElementById("res-msggg2").style.color="blue";
+        //document.writeln("Enter the tolerance :");
+    }
+    else if (z == "10") {
+        document.getElementById("res-msggg2").innerHTML = "Correct Tolerance Value";
+        document.getElementById("res-msggg2").style.color="green";
+       // document.writeln("Correct Tolerance Value");
+    }
+    else if (z != "10") {
+       document.getElementById("res-msggg2").innerHTML = "Incorrect Tolerance value";
+       document.getElementById("res-msggg2").style.color="red";
+       document.getElementById("res-sbtn2").disabled=false;
+       //  document.writeln("Incorrect Tolerance value");
+    }
+}
+
+//------------------------------------------------- Resistor 3  ---------------------------------------//
+function resfun(){
+    var x=document.getElementById("r3").value;
+    var y=document.getElementById("res-select3");
+  //  document.getElementById("res-msg3").innerHTML=y;
+    var w=y.options[y.selectedIndex].value;
+    var z=document.getElementById("res-tol3").value;
+ //   document.getElementById("res-msg3").innerHTML=y;
+    
+    if(x==""){
+       document.getElementById("res-msg3").innerHTML = "Enter The Resistance "; 
+       document.getElementById("res-msg3").style.color="blue";
+      // document.writeln("Enter The Resistance :");
+    }else if(x=="97"){
+      document.getElementById("res-msg3").innerHTML = "Correct Resistance Value"; 
+      document.getElementById("res-msg3").style.color="green";
+      // document.writeln("Correct Resistance Value");
+    }
+    else if (x!= "97") {
+       document.getElementById("res-msg3").innerHTML ="Incorrect resistance value";
+       document.getElementById("res-msg3").style.color="red";
+       document.getElementById("res-sbtn3").disabled=false;
+         //document.writeln("Incorrect resistance value");
+    }
+     if (w == 0)
+    {
+        document.getElementById("res-msgg3").innerHTML ="Please select a unit ";
+        document.getElementById("res-msgg3").style.color="blue";
+        //document.writeln("Please select a unit");
+    }
+    else if (w == "1") {
+        document.getElementById("res-msgg3").innerHTML ="Correct Unit";
+        document.getElementById("res-msgg3").style.color="green";
+        //document.writeln("Correct  Unit Value");
+    }
+    else if (w != "1") {
+         document.getElementById("res-msgg3").innerHTML ="Incorrect Unit";
+         document.getElementById("res-msgg3").style.color="red";
+         document.getElementById("res-sbtn3").disabled=false;
+         // document.writeln("Incorrect Unit");
+    }
+
+   if (z == "") {
+
+        document.getElementById("res-msggg3").innerHTML = "Enter the tolerance "; 
+        document.getElementById("res-msggg3").style.color="blue";
+        //document.writeln("Enter the tolerance :");
+    }
+    else if (z == "20") {
+       document.getElementById("res-msggg3").innerHTML = "Correct  Tolerance Value";
+       document.getElementById("res-msggg3").style.color="green";
+        // document.writeln("Correct  Tolerance Value");
+    }
+    else if (z != "20") {
+        document.getElementById("res-msggg3").innerHTML = "Incorrect Tolerance value";
+        document.getElementById("res-msggg3").style.color="red";
+        document.getElementById("res-sbtn3").disabled=false;
+       // document.writeln("Incorrect Tolerance value");
+    }
+}
+//------------------------------------------------- Resistor 4  ---------------------------------------//
+function resfunc(){
+    var x=document.getElementById("r4").value;
+    var y=document.getElementById("res-select4");
+  //  document.getElementById("res-msg4").innerHTML=y;
+    var w=y.options[y.selectedIndex].value;
+    var z=document.getElementById("res-tol4").value;
+ //   document.getElementById("res-msg4").innerHTML=y;
+    
+    if(x==""){
+       document.getElementById("res-msg4").innerHTML = "Enter The Resistance "; 
+       document.getElementById("res-msg4").style.color="blue";
+      // document.writeln("Enter The Resistance :");
+    }else if((x=="3.3")||(x=="3300")){
+       document.getElementById("res-msg4").innerHTML = "Correct Resistance Value"; 
+       document.getElementById("res-msg4").style.color="green";
+     // document.writeln("Correct Resistance Value");
+    }
+    else if ((x!= "3.3")  ){
+        document.getElementById("res-msg4").innerHTML ="Incorrect resistance value";
+        document.getElementById("res-msg4").style.color="red";
+        document.getElementById("res-sbtn4").disabled=false;
+}
+
+
+ else if ((x!= "3300")  ){
+     document.getElementById("res-msg4").innerHTML ="Incorrect resistance value";
+     document.getElementById("res-msg4").style.color="red";
+     document.getElementById("res-sbtn4").disabled=false;
+}
+
+
+
+     if (w == 0)
+    {
+        document.getElementById("res-msgg4").innerHTML ="Please select a unit ";
+        document.getElementById("res-msgg4").style.color="blue";
+       // document.writeln("Please select a unit");
+    }
+    else if ((x== "3.3") && (w!="2") ){
+        document.getElementById("res-msgg4").innerHTML ="Incorrect Unit value";
+        document.getElementById("res-msgg4").style.color="red";
+        document.getElementById("res-sbtn4").disabled=false;
+    }
+    else if ((x== "3300") && (w!="1") ){
+        document.getElementById("res-msgg4").innerHTML ="Incorrect Unit value";
+        document.getElementById("res-msgg4").style.color="red";
+        document.getElementById("res-sbtn4").disabled=false;
+    }
+
+    else if ((x== "3.3") && (w=="2") ){
+        document.getElementById("res-msg4").innerHTML = "Correct Resistance Value"; 
+       document.getElementById("res-msg4").style.color="green";
+        document.getElementById("res-msgg4").innerHTML ="Correct Unit value";
+        document.getElementById("res-msgg4").style.color="green";
+    }
+    else if ((x== "3300") && (w=="1") ){
+        document.getElementById("res-msg4").innerHTML = "Correct Resistance Value"; 
+       document.getElementById("res-msg4").style.color="green";
+        document.getElementById("res-msgg4").innerHTML ="Correct Unit value";
+        document.getElementById("res-msgg4").style.color="green";
+    }
+
+    
+    else if ((w != "2")|| (w!= "1")) {
+         document.getElementById("res-msgg4").innerHTML ="Incorrect Unit";
+         document.getElementById("res-msgg4").style.color="red";
+         document.getElementById("res-sbtn4").disabled=false;
+         // document.writeln("Incorrect Unit");
+    }
+
+   if (z == "") {
+
+        document.getElementById("res-msggg4").innerHTML = "Enter the tolerance "; 
+        document.getElementById("res-msggg4").style.color="blue";
+        //document.writeln("Enter the tolerance :");
+    }
+    else if (z == "0.1") {
+        document.getElementById("res-msggg4").innerHTML = "Correct Tolerance Value";
+        document.getElementById("res-msggg4").style.color="green";
+       // document.writeln("Correct Tolerance Value");
+    }
+    else if (z != "0.1") {
+        document.getElementById("res-msggg4").innerHTML = "Incorrect Tolerance value";
+        document.getElementById("res-msggg4").style.color="red";
+        document.getElementById("res-sbtn4").disabled=false;
+        //document.writeln("Incorrect Tolerance value");
+    }
+}
+//------------------------------------------------- Resistor 5  ---------------------------------------//
+function resfunct(){
+    var x=document.getElementById("r5").value;
+    var y=document.getElementById("res-select5");
+  //  document.getElementById("res-msg5").innerHTML=y;
+    var w=y.options[y.selectedIndex].value;
+    var z=document.getElementById("res-tol5").value;
+ //   document.getElementById("res-msg5").innerHTML=y;
+    
+    if(x==""){
+       document.getElementById("res-msg5").innerHTML = "Enter The Resistance "; 
+       document.getElementById("res-msg5").style.color="blue";
+       //document.writeln("Enter The Resistance :");
+    }else if(x=="158"){
+       document.getElementById("res-msg5").innerHTML = "Correct Resistance Value"; 
+       document.getElementById("res-msg5").style.color="green";
+      //document.writeln("Correct Resistance Value");
+    }
+    else if (x!= "158") {
+        document.getElementById("res-msg5").innerHTML ="Incorrect resistance value";
+        document.getElementById("res-msg5").style.color="red";
+        document.getElementById("res-sbtn5").disabled=false;
+        //document.writeln("Incorrect resistance value");
+    }
+     if (w == 0)
+    {
+        document.getElementById("res-msgg5").innerHTML ="Please select a unit ";
+        document.getElementById("res-msgg5").style.color="blue";
+        //document.writeln("Please select a unit");
+    }
+    else if (w == "4") {
+        document.getElementById("res-msgg5").innerHTML ="Correct Unit";
+        document.getElementById("res-msgg5").style.color="green";
+        //document.writeln("Correct Unit ");
+    }
+    else if (w != "4") {
+         document.getElementById("res-msgg5").innerHTML ="Incorrect Unit";
+         document.getElementById("res-msgg5").style.color="red";
+         document.getElementById("res-sbtn5").disabled=false;
+          //document.writeln("Incorrect Unit");
+    }
+
+   if (z == "") {
+
+        document.getElementById("res-msggg5").innerHTML = "Enter the tolerance "; 
+        document.getElementById("res-msggg5").style.color="blue";
+       // document.writeln("Enter the tolerance :");
+    }
+    else if (z == "2") {
+       document.getElementById("res-msggg5").innerHTML = "Correct Tolerance Value";
+       document.getElementById("res-msggg5").style.color="green";
+       // document.writeln("Correct Tolerance Value");
+    }
+    else if (z != "2") {
+       document.getElementById("res-msggg5").innerHTML = "Incorrect Tolerance value";
+       document.getElementById("res-msggg5").style.color="red";
+       document.getElementById("res-sbtn5").disabled=false;
+       // document.writeln("Incorrect Tolerance value");
+    }
+}
+//------------------------------------------------- Resistor 6  ---------------------------------------//
+function resfunt(){
+    var x=document.getElementById("r6").value;
+    var y=document.getElementById("res-select6");
+  //  document.getElementById("res-msg6").innerHTML=y;
+    var w=y.options[y.selectedIndex].value;
+    var z=document.getElementById("res-tol6").value;
+ //   document.getElementById("res-msg6").innerHTML=y;
+    
+    if(x==""){
+       document.getElementById("res-msg6").innerHTML = "Enter The Resistance "; 
+       document.getElementById("res-msg6").style.color="blue";
+       //document.writeln("Enter The Resistance :");
+    }else if(x=="615"){
+       document.getElementById("res-msg6").innerHTML = "Correct Resistance Value"; 
+       document.getElementById("res-msg6").style.color="green";
+     // document.writeln("Correct Resistance Value");
+    }
+    else if (x!= "615") {
+        document.getElementById("res-msg6").innerHTML ="Incorrect resistance value";
+        document.getElementById("res-msg6").style.color="red";
+        document.getElementById("res-sbtn6").disabled=false;
+       // document.writeln("Incorrect resistance value");
+    }
+     if (w == 0)
+    {
+        document.getElementById("res-msgg6").innerHTML ="Please select a unit ";
+        document.getElementById("res-msgg6").style.color="blue";
+        //document.writeln("Please select a unit");
+    }
+    else if (w == "4") {
+        document.getElementById("res-msgg6").innerHTML ="Correct Unit ";
+        document.getElementById("res-msgg6").style.color="green";
+        //document.writeln("Correct Unit ");
+    }
+    else if (w != "4") {
+         document.getElementById("res-msgg6").innerHTML ="Incorrect Unit";
+         document.getElementById("res-msgg6").style.color="red";
+         document.getElementById("res-sbtn6").disabled=false;
+         // document.writeln("Incorrect Unit");
+    }
+
+   if (z == "") {
+
+        document.getElementById("res-msggg6").innerHTML = "Enter the tolerance "; 
+        document.getElementById("res-msggg6").style.color="blue";
+       // document.writeln("Enter the tolerance :");
+    }
+    else if (z == "0.25") {
+        document.getElementById("res-msggg6").innerHTML = "Correct Tolerance Value";
+        document.getElementById("res-msggg6").style.color="green";
+       // document.writeln("Correct Tolerance Value");
+    }
+    else if (z != "0.25") {
+        document.getElementById("res-msggg6").innerHTML = "Incorrect Tolerance value";
+        document.getElementById("res-msggg6").style.color="red";
+        document.getElementById("res-sbtn6").disabled=false;
+        //document.writeln("Incorrect Tolerance value");
+    }
+}
+
+
+
+function showans1()
+{
+    document.getElementById('shw-msgr').style.visibility="visible";
+    document.getElementById('shw-msgu').style.visibility="visible";
+    document.getElementById('shw-msgt').style.visibility="visible";
+}
+function showans2()
+{
+    document.getElementById('shw-msgr2').style.visibility="visible";
+    document.getElementById('shw-msgu2').style.visibility="visible";
+    document.getElementById('shw-msgt2').style.visibility="visible";
+}
+function showans3()
+{
+    document.getElementById('shw-msgr3').style.visibility="visible";
+    document.getElementById('shw-msgu3').style.visibility="visible";
+    document.getElementById('shw-msgt3').style.visibility="visible";
+}
+function showans4()
+{
+    document.getElementById('shw-msgr4').style.visibility="visible";
+    document.getElementById('shw-msgu4').style.visibility="visible";
+    document.getElementById('shw-msgt4').style.visibility="visible";
+}
+function showans5()
+{
+    document.getElementById('shw-msgr5').style.visibility="visible";
+    document.getElementById('shw-msgu5').style.visibility="visible";
+    document.getElementById('shw-msgt5').style.visibility="visible";
+}
+
+function showans6()
+{
+    document.getElementById('shw-msgr6').style.visibility="visible";
+    document.getElementById('shw-msgu6').style.visibility="visible";
+    document.getElementById('shw-msgt6').style.visibility="visible";
+}
+//};*/
---
+++ theory.md
@@ -0,0 +1,152 @@
+# Theory
+
+## Types of Resistors
+
+<div align="center">
+<img src="images/fig1.png" width="30%">
+<p>Figure: 1 </p>
+</div>
+
+## Reading Value of Fixed Resistors
+<div align="center">
+<img src="images/fig2_1.png" width="30%">
+<p>Figure: 2.1 </p>
+</div>
+
+- Resistors are color coded as they are too small for the value to be written on them.
+- There are 4 or 5 bands of color . Value of a Resistor is decoded from these band of colors.
+
+<div align="center">
+<img src="images/fig2.png" width="30%">
+<p>Figure: 2.2 </p>
+</div>
+
+### Reading Value:Step 1
+
+- If your resistor has four color bands ,turn the resistor so that the gold or silver band is on right hand side or the end with more bands should point left.
+
+<div align="center">
+<img src="images/fig3.png" width="30%">
+<p>Figure: 3 </p>
+</div>
+
+### Reading Value:Step 2
+
+The first band is now on the left hand side. This represents the first digit .Based on the color make a note of the digit.In this case- 4 band its ‘5’ and for 5 band its ‘2’.
+
+<div align="center">
+<img src="images/fig4.png" width="30%">
+<p>Figure: 4 </p>
+</div>
+
+### Reading Value :Step 3
+
+The second band represents the second digit. The colors represent the same numbers as did the first digit .In this case -4 band its’6’ and for 5 band its’3’.
+
+<div align="center">
+<img src="images/fig5.png" width="30%">
+<p>Figure: 5 </p>
+</div>
+
+### Reading Value : Step 4
+
+The third band divulges how many zeros to add/divide to the first two numbers –for a 4 band Resistor . In this case – 4 band its ‘4’ zeroes to be added . So value is 560K.
+
+<div align="center">
+<img src="images/fig6.png" width="30%">
+<p>Figure: 6 </p>
+</div>
+
+### Reading Value : Step 5
+
+The third band denotes the 3rd digit – for a 5 band Resistor. In this case -5 band its ‘7’ . So the value of the 5 band resistor is 237 Ohms as its multipier digit is ‘0.
+
+<div align="center">
+<img src="images/fig7.png" width="30%">
+<p>Figure: 7 </p>
+</div>
+
+### Tolerance
+
+The last band denotes the tolerance . So the value of the 4 band resistor it is +/- 10% while for the 5 band resistor it is +/- 1%.
+
+<div align="center">
+<img src="images/fig8.png" width="30%">
+<p>Figure: 8 </p>
+</div>
+
+- Tolerance of a Resistor is also an important property to consider .
+- A 100 ohm resistor with a 10 % tolerance can mean its value can be any fixed value between 90 to 110 Ohms.
+- A 120 Ohm resistor with a 10 % tolerance can mean its value can be any fixed value between 108 and 132 Ohms.
+- So there is some overlap between 100 Ohm and 120 Ohm resistance in terms of its limits.
+
+## Mnemonic to Remember
+
+<div align="center">
+<img src="images/fig9.png" width="30%">
+<p>Figure: 9.1 </p>
+</div>
+
+<div align="center">
+<img src="images/fig9_1.png" width="30%">
+<p>Figure: 9.2 </p>
+</div>
+
+### Carbon Film Resistors
+
+- Most general purpose ,cheap resistor
+- Tolerance of Resistance value is usually +/- 5%
+- Power ratings of 1/8 W ,1/4 W and ½ W are usually used
+- Con:Tend to be electrically noisy
+
+<div align="center">
+<img src="images/fig10.png" width="30%">
+<p>Figure: 10 </p>
+</div>
+
+### Metal Film Resistor
+
+- Used when higher tolerance is needed , ie more value.
+- They have about +/- 0.05% tolerance
+
+<div align="center">
+<img src="images/fig11.png" width="30%">
+<p>Figure: 11 </p>
+</div>
+
+### Wire Wound Resistors
+- A wire wound resistor is made of metal resistance wire, and because of this they can be manufactured to precise values
+- Also, high wattage resistors can be made by thick wire material
+- Wire wound resistors in a ceramic case are called as ceramic resistors
+- Wire wound resistors in a ceramic case are called as ceramic resistors
+
+## Color Code for Resistor
+<!--div align="left">                        
+    <img src="images/color-code.png" width="100%">                             
+</div-->
+
+Color | Digit | Multiplier | Tolerence%  | Color Band | 
+:--|:--|:--|:--|:--|
+Black | 0 |  $$  10^0 \quad Ω $$ | | <input type="text" style="background-color:black" > | 
+Brown | 1 |  $$10^1\quad Ω$$ | 1 | <input type="text" style="background-color:brown" > | 
+Red | 2 |  $$10^1 \quad Ω$$ | 2 | <input type="text" style="background-color:red" > | 
+Orange | 3 |  $$10^3 \quad Ω \quad (1\quad kΩ)$$ | | <input type="text" style="background-color:orange" > | 
+Yellow | 4 |  $$10^4 \quad Ω \quad (10\quad kΩ)$$ | | <input type="text" style="background-color:yellow" > | 
+Green | 5 |  $$10^5 \quad Ω \quad (100\quad kΩ)$$ | 0.5 | <input type="text" style="background-color:limegreen" > | 
+Blue | 6 |  $$10^6 \quad Ω \quad (1\quad MΩ)$$ | 0.25 | <input type="text" style="background-color:blue" > | 
+Violet | 7 |  $$10^7 \quad Ω \quad (10\quad MΩ)$$ | 0.1 | <input type="text" style="background-color:blueviolet" > | 
+Grey | 8 |  $$10^8 \quad Ω \quad (100\quad MΩ)$$ | 0.05 | <input type="text" style="background-color:grey" > | 
+White | 9 |  $$10^9 \quad Ω \quad (1\quad GΩ)$$ | | <input type="text" style="background-color:whitesmoke" > | 
+Gold | 0 |  $$10^{-1} \quad Ω$$ | 5 | <input type="text" style="background-color:gold" > | 
+Silver | 0 |  $$10^{-2} \quad Ω$$ | 10 | <input type="text" style="background-color:silver" > | 
+None | 0 |   | 20 | <input type="text" style="background-color:none" > | 
+
+
+
+
+### Slides of the above theory
+
+- Pdf:  [Measurements of  Resistance](./images/measureresis.pdf)
+
+
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
